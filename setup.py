98d6ace0ff03569462fb547f6415df4056a1ef60
FaroukDaboussi <faroukdaboussi2009@gmail.com>
2024-12-03 19:35:44 +0100

    V2 schemas for agregation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deepview-classes',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(include=['v1', 'v1.*', 'v2', 'v2.*']),
     install_requires=[
         'pydantic',
