ecce98e6937c6fed0e396ec03d66c9eedd4b0412
FaroukDaboussi <faroukdaboussi2009@gmail.com>
2024-12-08 17:31:39 +0100

    V3 tracker free

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deepview-classes',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(include=['v1', 'v1.*', 'v2', 'v2.*']),
     install_requires=[
         'pydantic',
